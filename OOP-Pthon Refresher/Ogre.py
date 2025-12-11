d8707eca03d40791e73ab45191f7547ac98add76
danial <trody2001@gmail.com>
2025-12-11 18:47:19 +0200

    OPP Upload

---
+++ OOP-Pthon Refresher/Ogre.py
@@ -0,0 +1,17 @@
+from Enemy import *
+import random
+class Ogre(Enemy):
+
+    def __init__(self,health_points=10,attack_damage=1):
+        super().__init__(type_of_enemy="Ogre",
+                         health_points=health_points
+                         ,attack_damage=attack_damage)
+
+    def talk(self):
+        print("Ogre is slamming hand all around")
+
+    def special_attack(self):
+        did_special_attack_work = random.random() < 0.20
+        if did_special_attack_work:
+            self.attack_damage += 4
+            print("Ogre gets angry and increase attack by 4")
